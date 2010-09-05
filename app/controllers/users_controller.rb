ec34c628034e41147366f0b2f0d12a91ae33f421
Kleber Infante <corujitot@gmail.com>
2010-09-05 17:27:36 -0300

    users pagination.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.find(:all, :order => :name)
+    #@users = User.find(:all, :order => :name)
+    @users = User.paginate :page => params[:page], :order => :name, :per_page => 4
 
     respond_to do |format|
       format.html # index.html.erb
